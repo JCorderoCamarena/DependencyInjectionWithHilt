907dff487577121c958c726410177d01942a6ef9
Jorge Cordero <jcordero@midtronics.com>
2022-07-01 13:14:50 -0600

    Initial Commit

---
+++ app/src/main/java/com/midtronics/android/library/hiltintegrationplayground/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.midtronics.android.library.hiltintegrationplayground
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
